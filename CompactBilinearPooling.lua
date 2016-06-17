9aedc472bac26116dbfef1841812c5229c6347b9
Jin Hwa Kim <jnhwkim@gmail.com>
2016-06-17 18:19:26 +0900

    bug fix on testcase

--- CompactBilinearPooling.lua
+++ CompactBilinearPooling.lua
@@ -17,7 +17,6 @@ function CompactBilinearPooling:reset()
    self.h = torch.Tensor()
    self.s = torch.Tensor()
    self.y = torch.Tensor()
-   -- self.output = torch.Tensor()
    self.gradInput = {}
    self.tmp = torch.Tensor()
 end
@@ -29,20 +28,17 @@ end
 
 function CompactBilinearPooling:psi()
    self.y:zero()
+   local batchSize = self.input[1]:size(1)
    for i=1,2 do
       if self.homogeneous then  -- using the same samples
-         for j=1,self.h[1]:size(#self.h[1]:size()) do
-           local y_ = self.y[i][self.h[1][j]]
-           self.y[i][self.h[1][j]] = y_ + self.s[1][j]*self.input[i][j]
-        end
+         self.y[i]:indexAdd(2,self.h[1],torch.cmul(self.s[1]:repeatTensor(batchSize,1),self.input[i]))
       else
-         local batchSize = self.input[1]:size(1)
          self.y[i]:indexAdd(2,self.h[i],torch.cmul(self.s[i]:repeatTensor(batchSize,1),self.input[i]))
       end
    end
 end
 
-function CompactBilinearPooling:conv(res,x,y)
+function CompactBilinearPooling:conv(x,y)
    local batchSize = x:size(1)
    local dim = x:size(2)
    local function makeComplex(x,y)
@@ -54,12 +50,12 @@ function CompactBilinearPooling:conv(res,x,y)
    makeComplex(x,y)
    self.fft_x = self.fft_x or torch.CudaTensor(batchSize,1,1,dim,2)
    self.fft_y = self.fft_y or torch.CudaTensor(batchSize,1,1,dim,2)
-   res = res or torch.CudaTensor()
-   res:resize(batchSize,1,1,dim*2)
+   local output = output or torch.CudaTensor()
+   output:resize(batchSize,1,1,dim*2)
    cufft.fft1d(self.x_:view(x:size(1),1,1,-1), self.fft_x)
    cufft.fft1d(self.y_:view(y:size(1),1,1,-1), self.fft_y)
-   cufft.ifft1d(self.fft_x:cmul(self.fft_y), res)
-   return res:view(batchSize,1,1,dim,2)[{{},{1},{1},{1}}]:squeeze()
+   cufft.ifft1d(self.fft_x:cmul(self.fft_y), output)
+   return output:resize(batchSize,1,1,dim,2):select(2,1):select(2,1):select(3,1)
 end
 
 function CompactBilinearPooling:updateOutput(input)
@@ -67,11 +63,12 @@ function CompactBilinearPooling:updateOutput(input)
    self.input = input
    local inputSizes1 = input[1]:size()
    local inputSizes2 = input[2]:size()
-   local sizes1 = inputSizes1[#inputSizes1]
-   local sizes2 = inputSizes2[#inputSizes2]
-   self.h:resize(2, sizes1)
-   self.s:resize(2, sizes1)
-   self:sample()
+
+   if 0==#self.h:size() then
+      self.h:resize(2, inputSizes1[#inputSizes1])
+      self.s:resize(2, inputSizes2[#inputSizes2])
+      self:sample()  -- samples are fixed
+   end
 
    if 2 > #inputSizes1 then  -- no batch
       self.y:resize(2, self.outputSize)
@@ -84,7 +81,7 @@ function CompactBilinearPooling:updateOutput(input)
    if self.debug then print('pre:', sys.toc(1)) end
    self:psi()
    if self.debug then print('psi:', sys.toc(1)) end
-   self:conv(self.output, self.y[1], self.y[2])
+   self.output = self:conv(self.y[1], self.y[2])
    if self.debug then print('conv:', sys.toc(1)) end
 
    return self.output
@@ -102,9 +99,9 @@ function CompactBilinearPooling:updateGradInput(input, gradOutput)
       self.tmp:resizeAs(gradOutput)
 
       if 1==k then
-         self.tmp = self:conv(self.tmp, self.y[k%2+1], gradOutput)
+         self.tmp = self:conv(self.y[k%2+1], gradOutput)
       else
-         self.tmp = self:conv(self.tmp, gradOutput, self.y[k%2+1])
+         self.tmp = self:conv(gradOutput, self.y[k%2+1])
       end
       self.gradInput[k]:index(self.tmp, 2, self.h[k])
       self.gradInput[k]:cmul(self.s[k]:repeatTensor(batchSize,1))
--- test/test.lua
+++ test/test.lua
@@ -6,35 +6,39 @@ local precision = 1e-5
 
 function cbptest.testPsi()
    local homogeneous = true
+   local batch = 1
    local dim = 5
-   local S = 1000
-   local c = nn.CompactBilinearPooling(dim, homogeneous)
-   local x = torch.rand(dim):cuda()
-   local y = torch.rand(dim):cuda()
+   local S = 100
+   local x = torch.rand(batch,dim):cuda()
+   local y = torch.rand(batch,dim):cuda()
    local tmp = 0
+   local c
    for s=1,S do
+      c = nn.CompactBilinearPooling(dim, homogeneous):cuda()
       c:forward({x,y})
-      tmp = tmp + c.y[1]:dot(c.y[2])
+      tmp = tmp + c.y[1][1]:dot(c.y[2][1])
    end
-   local diff = math.abs(tmp/S - x*y)
-   assert(diff / (x*y) < .1, 'error_ratio='..diff / (x*y)..', E[<phi(x,h,s),phi(y,h,s)>]=<x,y>')
+   local xy = x[1]:dot(y[1])
+   local diff = math.abs(tmp/S - xy)
+   assert(diff / xy < .1, 'error_ratio='..diff / xy..', E[<phi(x,h,s),phi(y,h,s)>]=<x,y>')
 end
 
 function cbptest.testConv()
-   local x = torch.Tensor{{1,2,3},{2,3,4}}
-   local y = torch.Tensor{{1,1,1},{2,2,2}}
+   local x = torch.CudaTensor{{1,2,3},{2,3,4}}
+   local y = torch.CudaTensor{{1,1,1},{2,2,2}}
    local c = nn.CompactBilinearPooling(x:size(2))
-   local ans = torch.Tensor{{6,6,6},{18,18,18}}
-   c:conv(x,y)
-   ans:add(-c.output)
+   local ans = torch.CudaTensor{{6,6,6},{18,18,18}}
+   local output = torch.CudaTensor()
+   output = c:conv(x,y)  -- cuda only
+   ans:add(-output)
    assert(ans:norm() < precision, ans:norm())
 
-   local x = torch.Tensor{{1,2,3,1,1},{2,3,4,1,1}}
-   local y = torch.Tensor{{1,1,1,1,1},{2,2,2,1,1}}
+   local x = torch.CudaTensor{{1,2,3,1,1},{2,3,4,1,1}}
+   local y = torch.CudaTensor{{1,1,1,1,1},{2,2,2,1,1}}
    local c = nn.CompactBilinearPooling(x:size(2))
-   local ans = torch.Tensor{{8,8,8,8,8},{20,19,17,15,17}}
-   c:conv(x,y)
-   ans:add(-c.output)
+   local ans = torch.CudaTensor{{8,8,8,8,8},{17.5,17.5,17.75,17.75,17.5}}
+   output = c:conv(x,y)
+   ans:add(-output)
    assert(ans:norm() < precision, ans:norm())
 end
 
