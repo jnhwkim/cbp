2e15d3f87e6f911a3321c1291e6d1104f02f82cc
Jin Hwa Kim <jnhwkim@gmail.com>
2016-06-19 20:24:12 +0900

    add a simple testcase

--- test/test.lua
+++ test/test.lua
@@ -42,17 +42,38 @@ function cbptest.testConv()
    assert(ans:norm() < precision, ans:norm())
 end
 
-function cbptest.testGrad()
-   -- local x = torch.Tensor{{1,2,3},{2,3,4}}
-   -- local y = torch.Tensor{{1,1,1},{2,2,2}}
-   -- local c = nn.CompactBilinearPooling(x:size(2))
-   -- local diff = torch.Tensor{{6,6,6},{18,18,18}}
-   -- c:forward{x:add(torch.rand(2,3):mul(precision)),y:add(torch.rand(2,3):mul(precision))}
-   -- diff:add(-c.output):div(precision)
-   -- c:backward({x:add(torch.rand(2,3):mul(precision)),y:add(torch.rand(2,3):mul(precision))}, torch.Tensor(2,3):fill(1))
-   -- local grad = c.gradInput
-   -- local ans = diff:add(-grad)
-   -- assert(ans:norm() < precision, ans:norm())
+function cbptest.testLearning()
+   cutorch.manualSeed(123)
+   local N = 100
+   local C = 3
+   local iter = 100
+   local lr = .05
+   local x = torch.rand(N,1)
+   local y = torch.rand(N,1)
+   local t = torch.pow(x,2) + torch.pow(y,2) + torch.cmul(x,y)
+
+   x=x:cuda()
+   y=y:cuda()
+   t=t:cuda()
+
+   local model = nn.Sequential()
+      :add(nn.CompactBilinearPooling(C))
+      :add(nn.SignedSquareRoot())
+      :add(nn.Linear(C,1))
+   local criterion = nn.MSECriterion()
+
+   model=model:cuda()
+   criterion=criterion:cuda()
+
+   for i=1,iter do
+      local output = model:forward{x,y}
+      J = criterion:forward(output, t)
+      local dt = criterion:backward(output, t)
+      model:zeroGradParameters()
+      model:backward({x,y},dt)
+      model:updateParameters(lr)
+   end
+   assert(J < .2, 'CBP failed to learn (J='..J..')')
 end
 
 function cbp.test(tests)
