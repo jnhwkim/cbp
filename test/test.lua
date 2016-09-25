c0ddc2b5d35f76338fdec457241982ab43d66c15
takatosp1 <takatosp1@msn.com>
2016-09-25 18:05:18 -0400

    Update test.lua

--- test/test.lua
+++ test/test.lua
@@ -100,7 +100,7 @@ function cbptest.testLearning()
    assert(J < .2, 'CBP failed to learn (J='..J..')')
 end
 
-function cbp.test(tests, _debug)
+function cbptest.test(tests, _debug)
    debug = _debug or false
    mytester = torch.Tester()
    mytester:add(cbptest)
